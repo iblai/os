ddf1a0f74ab13c39975e4a30b9e9e34bbcd3dde0
null-crafter <163591881+null-crafter@users.noreply.github.com>
2026-03-23 20:00:33 +0000

    feat: add disableEmbedNavbar toggle in query param

--- app/platform/[tenantKey]/[mentorId]/_components/nav-bar/index.tsx
+++ app/platform/[tenantKey]/[mentorId]/_components/nav-bar/index.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Image from "next/image";
-import { useParams, usePathname } from "next/navigation";
+import { useParams, usePathname, useSearchParams } from "next/navigation";
 
 import {
   PenSquare,
@@ -503,6 +503,9 @@ export function NavBar() {
   const [forkMentor, { isLoading: isForkingMentor }] = useForkMentorMutation();
 
   const [editMentor] = useEditMentorMutation();
+  const searchParams = useSearchParams();
+  const rawValue = searchParams.get("disableEmbedNavbar");
+  const disableEmbedNavbar = rawValue === "1" || rawValue === "true";
 
   const handleModifyMentor = async () => {
     if (!tenantKey || !mentorId || !username) {
@@ -617,6 +620,9 @@ export function NavBar() {
 
   const visibleToLoggedInUsersOnly = !isAccessingPublicRoute || isLoggedIn();
 
+  if (disableEmbedNavbar) {
+    return <></>;
+  }
   if (embedMode) {
     return (
       <EmbedNavBar
