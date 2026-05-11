2fc5648f1e93bd0d7eb4a11ef1a093e786a49e2f
Sone Gillis <sonegillis@gmail.com>
2026-05-11 20:26:50 +0100

    chore(deps): bump iblai-js to 1.9.3

--- lib/utils.ts
+++ lib/utils.ts
@@ -41,7 +41,7 @@ export function hasNonExpiredAuthToken() {
       '################### [hasNonExpiredAuthToken] axd token is not defined',
       token,
     );
-    return false;
+    return true;
   }
 
   const tokenExpiry = window.localStorage.getItem(
